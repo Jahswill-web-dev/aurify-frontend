3e74ca76f699632f764e3e923438ef63b8c5f35a
Jahswill-web-dev <onuegbujahswill@gmail.com>
2024-07-22 20:12:41 +0100

    working on signup page -2

--- src/app/signup/_components/creatAccount.js
+++ src/app/signup/_components/creatAccount.js
@@ -2,32 +2,32 @@ function CreateAccount() {
   return (
     <div>
       <form>
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-4 mt-4">
           <div>
-            <p>User Name</p>
+            <p className="text-p-text-darker pb-2 text-lg">User Name</p>
             <input
               type="email"
               id="email"
               placeholder="Johndoe@gmail.com"
-              className="focus:outline-none"
+              className="focus:border-primary border-p-text-darker w-64 h-[35px] text-x-sub-head border-2"
             ></input>
           </div>
           <div>
-            <p>Email</p>
+            <p className="text-p-text-darker pb-2 text-lg">Email</p>
             <input
               type="password"
               id="password"
               placeholder=""
-              className="focus:outline-none"
+              className="focus:border-primary border-p-text-darker w-64 h-[35px] text-x-sub-head border-2"
             ></input>
           </div>
           <div>
-            <p>Password</p>
+            <p className="text-p-text-darker pb-2 text-lg">Password</p>
             <input
               type="password"
               id="confirm password"
               placeholder=""
-              className="focus:outline-none"
+              className="focus:border-primary border-p-text-darker w-64 h-[35px] text-x-sub-head border-2"
             ></input>
           </div>
         </div>
